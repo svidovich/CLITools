5e7b4dde7d0df65c43884b79ebe414b51c083050
vivec <samuel.vidovich@gmail.com>
2018-07-10 23:17:46 -0400

    Added some comments to clarify confusing ass bash

--- bash/counter/counter.sh
+++ bash/counter/counter.sh
@@ -3,10 +3,13 @@
 #GREEN = '\033[0;32m'
 for (( i = 1; i <= 100; i++  )); 
 do
+	# This line makes the counter update in place
 	printf '\r%2d' "$i";
+	# This line prints the total number after it
 	printf "/100";
 	sleep 0.1;
 	if
+		# When the counter is done, let's print done in green text
 		[ $i == 100 ]
 		then
 			tput setaf 2; echo -e "\nDone.\n";
