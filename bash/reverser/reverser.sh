572740fc39b9812f8a49cd5264600b87cec8cf5d
vivec <samuel.vidovich@gmail.com>
2018-07-12 23:09:15 -0400

    Finished reverser

--- bash/reverser/reverser.sh
+++ bash/reverser/reverser.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-
+string=$(<thefile.txt)
+printf "Text to be reversed:\n$string\n"
+length=`expr length "$string"`
+printf "The reversed text:\n"
+for (( i=-1; i>-$length-1; i-- ));
+do
+	printf "${string:$i:1}";
+done
+printf "\n"
