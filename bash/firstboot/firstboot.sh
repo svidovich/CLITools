5192a1b7bd2c0462023bc7fe607848b9cb0b919b
vivec <samuel.vidovich@gmail.com>
2018-07-12 23:30:40 -0400

    Made a handy tool for updating or a first time startup of a linux machine.

---
+++ bash/firstboot/firstboot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# We assume that you are connected to the internet.
+printf "Would you like to update, or install popular programs?\n1. Update\n2. Install\n"
+read choice
+if [ $choice == 1 ]
+then
+	apt update
+	apt upgrade
+elif [ $choice == 2 ]	
+then
+	apt update
+	apt upgrade
+	apt install vim tmux docker python python3-pip virtualenv
+else
+	printf "\nBad input.\n"
+fi
