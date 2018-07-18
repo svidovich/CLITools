b75d2eb569f3a1dc9a45ea1c230aa1fc7658ffee
vivec <samuel.vidovich@gmail.com>
2018-07-17 21:16:18 -0400

    tadah!

--- bash/badmusic/song.sh
+++ bash/badmusic/song.sh
@@ -6,4 +6,6 @@ sleep 1;
 beep -f 466 -l 400 -n -f 440 -l 400 -n -f 466 -l 400 -n -f 392 -l 400 -n -f 294 -l 800
 
 sleep 1;
-beep -f 440 -l 400 -n -f 415 -l 400 -n -f 440 -l 400 -n -f 349 -l 400 -n -f 293.7 -l 400
+beep -f 440 -l 400 -n -f 415 -l 400 -n -f 440 -l 400 -n -f 349 -l 400 -n -f 262 -l 1200 
+sleep 0.5;
+beep -f 440 -l 1200 -n -f 392 -l 1200
