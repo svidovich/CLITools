e2d2e699c2c78a7b157bed77215af4b95e1a0536
vivec <samuel.vidovich@gmail.com>
2018-07-20 22:20:30 -0400

    Added menu options, character array

--- C/rainbowtesting.c
+++ C/rainbowtesting.c
@@ -5,9 +5,22 @@
 
 WINDOW *createwindow(int height, int width, int y0, int x0);
 void removewindow(WINDOW *the_window);
+void buildmenu(WINDOW *the_window, int n);//, char *string)
 
 int main ()
 {
+	char *choices[]
+	{
+		"I",
+		"II",
+		"III",
+		"IV",
+		"Exit"
+	}
+	// sizeof(choices) gives the size of an array of character pointers.
+	// Character pointers are bigger than 1.
+	// So, we need to divide by the size of a character pointer.
+	int n = sizeof(choices)/sizeof(char *);
 	WINDOW *tl;
 	WINDOW *tr; 
 	WINDOW *bl;
@@ -18,13 +31,19 @@ int main ()
 	initscr();
 	cbreak();
 //	raw();
-//	noecho();
+	noecho();
 	getmaxyx(stdscr, y, x);
 	refresh();
 	tl = createwindow(y/2,x/2, 0, 0);
 	tr = createwindow(y/2,x/2, 0, x/2);
 	bl = createwindow(y/2,x/2, y/2, 0);
 	br = createwindow(y/2,x/2, y/2, x/2);
+	keypad(tl, TRUE);
+	keypad(tr, TRUE);
+	keypad(bl, TRUE);
+	keypad(br, TRUE);
+	refresh();
+	buildmenu(tl,1);
 	refresh();
 	getch();
 	delwin(tl);
@@ -41,7 +60,7 @@ WINDOW *createwindow(int height, int width, int y0, int x0)
 	WINDOW *the_window;
 	the_window = newwin(height, width, y0, x0);
 	// We're going to put a box around the window.
-	box(the_window, 0, 0);
+	wborder(the_window, ':', ':', '~', '~', 'x', 'x', 'x', 'x');
 	wrefresh(the_window);
 	return the_window;
 }
@@ -50,3 +69,37 @@ void removewindow(WINDOW *the_window)
 {
 	delwin(the_window);
 }
+
+// Builds a menu with n options
+void buildmenu(WINDOW *the_window, int n)//, char *string)
+{
+	int i, x, y;
+	n++;
+	getmaxyx(the_window, y, x);
+	mvwprintw(the_window, y/2, x/2, "%s", "Menu Item");
+	
+//	for ( i = 0; i < n; i++)
+//	{
+//		wattron(the_window, A_BOLD);
+//		mvwprint()
+//	}
+	wrefresh(the_window);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
