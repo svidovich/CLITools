d4b65ebe3aaded71af254a59737a80a4237320b6
vivec <samuel.vidovich@gmail.com>
2018-07-23 23:55:08 -0400

    Added firework to effectlib

--- C/effectlib.c
+++ C/effectlib.c
@@ -6,7 +6,133 @@
 // Requires mousemask(EVENT_NAME).
 // Recommends curs_set(0) so that the cursor is invisible.
 void starburst(MEVENT mouseevent);
+void firework(MEVENT mouseevent);
 
+// Firework: Takes a mouse event and prints a cute firework at the event location.
+// Requires mousemast (EVENT_NAME).
+// Recommends curs_set(0) so that the cursor is invisible.
+void firework(MEVENT mouseevent)
+{
+
+	move(mouseevent.y + 1, mouseevent.x);
+	printw(".");
+	move(mouseevent.y - 1 , mouseevent.x);
+	printw(".");
+	move(mouseevent.y, mouseevent.x + 1);
+	printw(".");
+	move(mouseevent.y, mouseevent.x - 1);
+	printw(".");
+	move(mouseevent.y + 1, mouseevent.x + 1);
+	printw(".");
+	move(mouseevent.y + 1, mouseevent.x - 1);
+	printw(".");
+	move(mouseevent.y - 1, mouseevent.x + 1);
+	printw(".");
+	move(mouseevent.y - 1, mouseevent.x - 1);
+	printw(".");
+	refresh();
+	usleep(250000);
+
+	move(mouseevent.y + 2, mouseevent.x);
+	printw(".");
+	move(mouseevent.y - 2 , mouseevent.x);
+	printw(".");
+	move(mouseevent.y, mouseevent.x + 2);
+	printw(".");
+	move(mouseevent.y, mouseevent.x - 2);
+	printw(".");
+	move(mouseevent.y + 2, mouseevent.x + 2);
+	printw(".");
+	move(mouseevent.y + 2, mouseevent.x - 2);
+	printw(".");
+	move(mouseevent.y - 2, mouseevent.x + 2);
+	printw(".");
+	move(mouseevent.y - 2, mouseevent.x - 2);
+	printw(".");
+	refresh();
+	usleep(250000);
+
+
+	move(mouseevent.y + 3, mouseevent.x);
+	printw("*");
+	move(mouseevent.y - 3 , mouseevent.x);
+	printw("*");
+	move(mouseevent.y, mouseevent.x + 3);
+	printw("*");
+	move(mouseevent.y, mouseevent.x - 3);
+	printw("*");
+	move(mouseevent.y + 3, mouseevent.x + 3);
+	printw("*");
+	move(mouseevent.y + 3, mouseevent.x - 3);
+	printw("*");
+	move(mouseevent.y - 3, mouseevent.x + 3);
+	printw("*");
+	move(mouseevent.y - 3, mouseevent.x - 3);
+	printw("*");
+	refresh();
+	usleep(250000);
+
+
+	move(mouseevent.y + 1, mouseevent.x);
+	printw(" ");
+	move(mouseevent.y - 1 , mouseevent.x);
+	printw(" ");
+	move(mouseevent.y, mouseevent.x + 1);
+	printw(" ");
+	move(mouseevent.y, mouseevent.x - 1);
+	printw(" ");
+	move(mouseevent.y + 1, mouseevent.x + 1);
+	printw(" ");
+	move(mouseevent.y + 1, mouseevent.x - 1);
+	printw(" ");
+	move(mouseevent.y - 1, mouseevent.x + 1);
+	printw(" ");
+	move(mouseevent.y - 1, mouseevent.x - 1);
+	printw(" ");
+	refresh();
+	usleep(100000);
+
+	move(mouseevent.y + 2, mouseevent.x);
+	printw(" ");
+	move(mouseevent.y - 2 , mouseevent.x);
+	printw(" ");
+	move(mouseevent.y, mouseevent.x + 2);
+	printw(" ");
+	move(mouseevent.y, mouseevent.x - 2);
+	printw(" ");
+	move(mouseevent.y + 2, mouseevent.x + 2);
+	printw(" ");
+	move(mouseevent.y + 2, mouseevent.x - 2);
+	printw(" ");
+	move(mouseevent.y - 2, mouseevent.x + 2);
+	printw(" ");
+	move(mouseevent.y - 2, mouseevent.x - 2);
+	printw(" ");
+	refresh();
+	usleep(100000);
+
+
+	move(mouseevent.y + 3, mouseevent.x);
+	printw(" ");
+	move(mouseevent.y - 3 , mouseevent.x);
+	printw(" ");
+	move(mouseevent.y, mouseevent.x + 3);
+	printw(" ");
+	move(mouseevent.y, mouseevent.x - 3);
+	printw(" ");
+	move(mouseevent.y + 3, mouseevent.x + 3);
+	printw(" ");
+	move(mouseevent.y + 3, mouseevent.x - 3);
+	printw(" ");
+	move(mouseevent.y - 3, mouseevent.x + 3);
+	printw(" ");
+	move(mouseevent.y - 3, mouseevent.x - 3);
+	printw(" ");
+	refresh();
+	usleep(100000);
+
+
+}
 
 
 void starburst(MEVENT mouseevent)
