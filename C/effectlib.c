3046792590427843cb5314d5fb062a8c36ffa5ce
vivec <samuel.vidovich@gmail.com>
2018-07-24 22:01:46 -0400

    Added screenborder to effects

--- C/effectlib.c
+++ C/effectlib.c
@@ -6,11 +6,80 @@
 // Requires mousemask(EVENT_NAME).
 // Recommends curs_set(0) so that the cursor is invisible.
 void starburst(MEVENT mouseevent);
-void firework(MEVENT mouseevent);
 
 // Firework: Takes a mouse event and prints a cute firework at the event location.
 // Requires mousemast (EVENT_NAME).
 // Recommends curs_set(0) so that the cursor is invisible.
+void firework(MEVENT mouseevent);
+
+// Screenborder: Takes a winow and makes a cute rainbow border in it given a user's
+// choice of letter. Argument is any window.
+void screenborder(WINDOW *the_window);
+
+
+void screenborder(WINDOW *the_window) {
+	srand(time(0));
+	int ch;
+	int a, b, x, y;
+	start_color();
+ 	init_pair(0, COLOR_WHITE, COLOR_BLACK);
+	init_pair(1, COLOR_RED, COLOR_BLACK);
+	init_pair(2, COLOR_GREEN, COLOR_BLACK);
+	init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+ 	init_pair(4, COLOR_BLUE, COLOR_BLACK);
+	init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
+	init_pair(6, COLOR_CYAN, COLOR_BLACK);
+	init_pair(7, COLOR_WHITE, COLOR_BLACK);
+	noecho();
+	getmaxyx(the_window, y, x);
+	ch = getch();
+	for ( a = 0; a < y; a++)
+	{
+		for ( b = 0; b < x; b++)
+		{
+			if (a == 0)
+			{
+				attron(A_BOLD);
+				mvwaddch(the_window,a,b,ch | COLOR_PAIR(rand() % 8));
+				attroff(A_BOLD);
+				wrefresh(the_window);
+				usleep(900);
+
+			}
+			else if (a == y-1)
+			{
+				attron(A_BOLD);
+				mvwaddch(the_window,a,b,ch | COLOR_PAIR(rand() % 8));
+				attroff(A_BOLD);
+				wrefresh(the_window);
+				usleep(900);
+			}
+			else if (b == 0)
+			{
+				attron(A_BOLD);
+				mvwaddch(the_window,a,b,ch | COLOR_PAIR(rand() % 8));
+				attroff(A_BOLD);
+				wrefresh(the_window);
+				usleep(900);
+			}
+			else if (b == x-1)
+			{
+				attron(A_BOLD);
+				mvwaddch(the_window,a,b,ch | COLOR_PAIR(rand() % 8));
+				attroff(A_BOLD);
+				wrefresh(the_window);
+				usleep(900);
+			}
+		}
+	}
+
+}
+
+
+
+
+
+
 void firework(MEVENT mouseevent)
 {
 
