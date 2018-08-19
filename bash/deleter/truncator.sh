e136f2f50dd2b4e2dba291db212dfc284e37a4bf
vivec <samuel.vidovich@gmail.com>
2018-08-18 23:19:58 -0400

    Better name, added comments

---
+++ bash/deleter/truncator.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This is Unix time at execution
+CURRENTTIME=`date +"%s"`
+# Find files only in subdirectories. You can change this by modifying
+# the mindepth parameter.
+THEFILES=`find . -mindepth 2 -type f`
+# For every file in the files found,
+for file in $THEFILES
+do
+	# Figure out when it was created
+	FILETIME=`date -r $file +"%s"`
+	# If it's more than 24 hours old,
+	if (( $CURRENTTIME - $FILETIME > 86400 ));
+	# Truncate it, but keep the file in place.
+	then > $file
+	fi
+done
