60b13ad6695855c251f11c51b3238312a3d4e92d
vivec <samuel.vidovich@gmail.com>
2018-08-22 23:34:15 -0400

    Added meminfo

--- bash/syswatch/syswatch.sh
+++ bash/syswatch/syswatch.sh
@@ -4,12 +4,14 @@ printf "Hello %s. Welcome to syswatch.\n" "$USER"
 run=true
 while [ $run ]
 	do
-		printf "Menu\n1. Check Memory Usage\n2. Exit\n"
+		printf "Menu\n1. Memory Info\n2. Exit\n"
 		printf "Make your selection: "
 		read input
 		printf "You selected %s.\n\n" "$input"
 		if [ "$input" = "2" ]; then
 			break
+		elif [ "$input" = "1" ]; then
+			cat /proc/meminfo
 		fi
 	done
 printf "\nGoodbye.\n"
