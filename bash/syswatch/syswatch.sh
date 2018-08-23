86d27accb928e114d8e17be80757fb8c829e0979
vivec <samuel.vidovich@gmail.com>
2018-08-22 23:46:36 -0400

    Added more info, processor query

--- bash/syswatch/syswatch.sh
+++ bash/syswatch/syswatch.sh
@@ -4,15 +4,21 @@ printf "Hello %s. Welcome to syswatch.\n" "$USER"
 run=true
 while [ $run ]
 	do
-		printf "Menu\n1. Memory Info\n2. Disk Usage\n3. Exit\n"
+		printf "Menu\n1. Memory Info\n2. Disk Usage\n3. Processor Usage\n4. Exit\n"
 		printf "Make your selection: "
 		read input
-		if [ "$input" = "3" ]; then
+		if [ "$input" = "4" ]; then
 			break
 		elif [ "$input" = "1" ]; then
+			printf "\nDoing cat /proc/meminfo:\n"
 			cat /proc/meminfo
 		elif [ "$input" = "2" ]; then
+			printf "\nDoing df -h:\n"
 			df -h
+		elif [ "$input" = "3" ]; then
+			printf "\nDoing ps -eo pid,pcpu | sort -k 1 -nr | head -10:\n"
+			printf "PID   PCPU\n"
+			ps -eo pid,pcpu | sort -k 1 -nr | head -10
 		fi
 	done
 printf "\nGoodbye.\n"
